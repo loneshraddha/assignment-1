d43c41256a1048b977d0f13cb8920a7d27344911
loneshraddha <90711462+loneshraddha@users.noreply.github.com>
2021-09-15 09:00:50 +0530

    Day 1 Assignment Submitted

---
+++ python.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+x=4.5
+y=4.5
+print(x is y) 
+a=3.25
+b=3.26
+print(a is b)
+string="shraddha"
+dell="rushikesh"
+print( string is dell) 
+print(1>3>4) 
+str='15'
+print("int('15')with base 2=",int(str,2))
+s=str
+print("value of str function=",s)
+float_string=float("-12")
+print(value of float function=",float_string")
+
